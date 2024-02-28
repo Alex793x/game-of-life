bdb4454957c2165040e4275b754aa72c242bdf5f
Alex Holmberg <Alex793x@stud.kea.dk>
2024-02-28 20:22:33 +0100

    Fixed apos error

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ const GameOfLifePage: React.FC = () => {
 
   return (
     <div>
-      <h1>Conway's Game of Life with Next.js, React, Typescript, and Tailwind CSS</h1>
+      <h1>Conway&apos;s Game of Life with Next.js, React, Typescript, and Tailwind CSS</h1>
       <GameOfLifeEngine/>
     </div>
   );
